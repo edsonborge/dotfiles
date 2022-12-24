a74e7380d782876ee8573ef7db14ae1606e8c6ff
Edson Borge <17258402+edsonborge@users.noreply.github.com>
2022-12-24 01:39:48 -0300

    feat: add post install scripts

---
+++ src/post-install/dev-essentials.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt install -y libssl-dev
+
+sudo apt install -y software-properties-common
+
+sudo apt install -y git
+
+sudo apt install -y vim
+
+sudo apt install -y curl
+
+sudo apt install -y fonts-firacode
+
+sudo apt install -y gnupg
+
+sudo apt install -y lm-sensors
+
+sudo apt install -y gnome-screenshot
---
+++ src/post-install/vscode.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Install vscode
+sudo apt install code
+
+# Install extensions
+
+# Lucy theme
+code --install-extension juliettepretot.lucy-vscode
+
+# Material icon theme
+code --install-extension PKief.material-icon-theme
+
+# Editor config
+code --install-extension EditorConfig.EditorConfig
+
+# Eslint
+code --install-extension dbaeumer.vscode-eslint
+
+# Prettier
+code --install-extension esbenp.prettier-vscode
+
+#Live server
+code --install-extension ritwickdey.LiveServer
+
+# Live share
+code --install-extension ms-vsliveshare.vsliveshare
+
+# Docker
+code --install-extension ms-azuretools.vscode-docker
+
+# GitLens
+code --install-extension eamodio.gitlens
+
+# Auto import
+code --install-extension steoates.autoimport
+
+# Color highlight
+code --install-extension naumovs.color-highlight
+
+# Sort lines
+code --install-extension Tyriar.sort-lines
+
+# Todo tree
+code --install-extension Gruntfuggly.todo-tree
+
+# Turbo console log
+code --install-extension ChakrounAnas.turbo-console-log
+
+# Version lens
+code --install-extension pflannery.vscode-versionlens
+
+# Vetur
+code --install-extension octref.vetur
