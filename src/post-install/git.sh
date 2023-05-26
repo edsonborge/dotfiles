1837b8a646ee0243326c8d9fad76dbd14fe5eea3
Edson Borge <17258402+edsonborge@users.noreply.github.com>
2023-05-26 16:12:19 -0300

    update git shell file

--- src/post-install/git.sh
+++ src/post-install/git.sh
@@ -8,4 +8,5 @@ git config --global --replace-all alias.s '!git status -s'
 git config --global --replace-all alias.c '!git add --all && git commit -m'
 git config --global --replace-all alias.l '!git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
 git config --global --replace-all alias.f '!git fetch --all --prune'
+git config --global --replace-all alias.ch '!git checkout'
 
