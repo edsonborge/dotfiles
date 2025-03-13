b907610d43bfbdd6a300366779268e3b5fa5025e
Edson Borge <17258402+edsonborge@users.noreply.github.com>
2025-03-13 18:37:35 -0300

    update docker installation script

--- src/post-install/docker.sh
+++ src/post-install/docker.sh
@@ -33,7 +33,7 @@ sudo apt-get update
 
 # Instalar Docker e Docker Compose
 echo "Instalando Docker e Docker Compsose..."
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Adicionar grupo docker
 echo "Adicionando grupo do docker..."
