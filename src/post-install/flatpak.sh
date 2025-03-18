e3b2a25d49a216dca6d4c8da4b47e7b4c5480c9c
Edson Borge <17258402+edsonborge@users.noreply.github.com>
2025-03-18 16:32:19 -0300

    update configs

--- src/post-install/flatpak.sh
+++ src/post-install/flatpak.sh
@@ -9,17 +9,11 @@ flatpak install -y flathub com.getpostman.Postman
 # Bitwarden
 flatpak install -y flathub com.bitwarden.desktop
 
-# Edge
-flatpak install -y flathub com.microsoft.Edge
-
 # Slack
 flatpak install -y flathub com.slack.Slack
 
 # Teams
 flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
-
-# Spotify
-flatpak install -y flathub com.spotify.Client
 
 # Discord
 flatpak install -y flathub com.discordapp.Discord
--- src/post-install/nvm.sh
+++ src/post-install/nvm.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-
 nvm install --lts
-
 npm install -g yarn
